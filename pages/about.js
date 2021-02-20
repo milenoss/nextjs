b80a222e701b6412e64ad3416c16b5919501a61b
Milen Khanal <milen.khanal@lego.com>
2021-02-20 19:38:50 +0000

    Add nav, footer, about, component folder

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const About = () => { 
+    return ( 
+        <div> 
+            <h1>About</h1>
+            <p>Where tech meets yoga</p>
+        </div>
+    )
+}
+export default About; 
---
+++ pages/ninjas/index.js
@@ -0,0 +1,8 @@
+const Ninjas = () => { 
+    return ( 
+        <div>
+            <h1>All ninjas</h1>
+        </div>
+    )
+}
+export default Ninjas; 
